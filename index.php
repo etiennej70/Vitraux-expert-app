f66b0a9fd8f5a2a43c7ad1e9b8ff6820ec1096cc
Etienne <etiennej70@hotmail.fr>
2016-06-09 11:09:07 +0200

    Ajout d'une page d'index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>POC vitraux experts</title>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Bienvenue, expert !</h2>
+    </div>
+  </div>
+</body>
+</html>
