ef3e81e51e34e2fc86b108baa6738f008fb23954
Etienne <etiennej70@hotmail.fr>
2016-06-16 14:41:05 +0200

    Modification de la fiche d'un vitrail avec suppression SDKPHP Facebook pour SDK JS et modification des appel webservices

--- fiche.php
+++ fiche.php
@@ -2,26 +2,6 @@
 //id du vitrail dans l'URL
 $id_vitrail = htmlspecialchars($_GET["vitrail"]);
 
-//On récupère les infos de la base concernant le vitrail
-$donnees = array();
-$data = json_decode(file_get_contents("http://argos2.hypertopic.org/corpus/Vitraux%20-%20B%C3%A9nel"), true);
-foreach ($data["rows"] as $cle => $valeur) {
-	if($valeur["id"] == $id_vitrail) {
-		$donnees[key($valeur["value"])] = $valeur["value"][key($valeur["value"])];
-	}
-}
-
-//API facebook
-session_start();
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
-
-//Facebok object of the app vitraux experts utt
-$fb = new Facebook\Facebook([
-  'app_id' => $config['facebook']['app_id'],
-  'app_secret' => $config['facebook']['app_secret'],
-  'default_graph_version' => $config['facebook']['default_graph_version'],
-]);
 ?>
 <!DOCTYPE html>
 <html>
@@ -42,42 +22,50 @@ $fb = new Facebook\Facebook([
 }(document, 'script', 'facebook-jssdk'));</script>
 
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-4 vitrail_img">
     	<!--On affiche l'image du vitrail-->
       <img src="<?php echo 'http://steatite.hypertopic.org/picture/'.$id_vitrail; ?>" class="img-responsive" />    
     </div>
-    <div class="col-md-8">
+    <div class="col-md-8 vitrail_data">
     	<h3>Données :</h3>
-    	<?php 
-    	//On affiche les données de la base
-    		if(empty($donnees)) {
-    			echo "<p>Aucune donnée en base</p>";
-    		}
-    		else {
-    			foreach ($donnees as $key => $value) {
-    				if($key == "topic") {
-    			     $donnesProfondes = json_decode(file_get_contents("http://argos2.hypertopic.org/topic/".$value ["viewpoint"]."/".$value['id']."/"), true);
-               $nuageDeTags = array();
-               foreach($donnesProfondes["rows"] as $cle => $valeur) {
-                  $tag = $valeur["value"]["item"]["name"];
-                  if($tag != "" && $tag != "Sans nom" && $tag != " ") {
-                    $nuageDeTags[] = $tag;
-                  }
-               }
-               echo "<p>";
-               echo "Tags :";
-               echo "<ul>";
-               foreach ($nuageDeTags as $tag) {
-                  echo "<li>".$tag."</li>";
-               }
-               echo "</ul>";
-               echo "</p>";
-    				}
-    				else {
-    					echo "<p>$key : $value</p>";
-    				}    				
-    			}
-    		}  		
+    	<?php
+      $donnees = array();
+      $tags_array = array();
+      //On récupère les infos de la base concernant le vitrail
+      $data = json_decode(file_get_contents("http://argos2.hypertopic.org/item/Vitraux%20-%20B%C3%A9nel/".$id_vitrail), true);
+      foreach ($data['rows'] as $key => $value) {
+        if(count($value['value']) == 1) {
+          echo '<p><b>'.ucfirst(key($value['value'])).'</b> : '.$value['value'][key($value['value'])].'</p>';
+        }
+        else {
+          //Sinon, il s'agit de mots clés, tags, etc. Il faut alors aller chercher le nom correspondant à l'ID du tag récupéré
+          if(isset($value['value']['topic'])) {
+            //On appel le webservice contenant cette information
+            $curl = curl_init('http://argos2.hypertopic.org/viewpoint/'.$value['value']['topic']['viewpoint']);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json; charset=utf-8","Accept:application/json, text/javascript, */*; q=0.01"));
+            $result = curl_exec($curl);
+            //On décode le JSON retourné par le webservice
+            $array_results = json_decode($result, true)['rows'];
+            //Pour chaque ligne, on compare par rapport à l'ID du point de vue et de l'id du tga que l'on a, pour voir si cela correspond
+            foreach ($array_results as $id_viewpoint => $item) {
+              if(isset($item['key'][1]) && $item['key'][0] == $value['value']['topic']['viewpoint'] && $item['key'][1] == $value['value']['topic']['id']) {
+                //Si on a la bonne info, on enregistre le tag dans un array pour pouvoir les afficher tous d'un coup par la suite
+                if(isset($item['value']['name'])) {
+                  $tags_array[] = $item['value']['name'];
+                }
+              }   
+            }
+          }     
+        }
+      }
+      //On affiche enfin les tags
+      echo '<h3>Tags :</h3>';
+      echo '<ul>';
+      foreach ($tags_array as $tag) {
+        echo "<li>".$tag."</li>";
+      }
+      echo '</ul>';
     	?>
     	<hr>
       <div class="fb-comments" data-href="http://vitraux-experts.dev.etiennejacquot.com/fiche.php?vitrail=<?php echo $id_vitrail ?>" data-width="100%" data-numposts="5"></div>
